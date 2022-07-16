f03597c10d2a0f2fbbf9bbd3efafe9add274e637
Deepankar Sharma <46363338+Deepankar1999@users.noreply.github.com>
2022-07-16 05:12:33 -0700

    Update database_connection.py

--- utils/database_connection.py
+++ utils/database_connection.py
@@ -6,11 +6,10 @@ class DatabaseConnection:
         self.conection=None
         self.host=host
     def __enter__(self):
-        self.conection=sqlite3.connect('self.host')
+        self.conection=sqlite3.connect(self.host)
         return self.conection
     def __exit__(self, exc_type, exc_val, exc_tb):
         if exc_tb or exc_val or exc_tb:
-            self.conection.close()
-        else:
+            
             self.conection.commit()
             self.connection.close()
